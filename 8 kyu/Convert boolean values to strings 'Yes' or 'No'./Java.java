660362669a8b758cd65a06607ce8bfba4b186daa
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-05-21 14:29:43 +0300

    Create Java.java

---
+++ 8 kyu/Convert boolean values to strings 'Yes' or 'No'./Java.java
@@ -0,0 +1,31 @@
+class YesOrNo
+{
+  public static String boolToWord(boolean b){
+    return b ? "Yes" : "No";
+    
+    
+    /* 
+    OR
+
+    if (b == true) {
+      return "Yes";
+    }else{
+      return "No";
+    }
+
+    */
+  }
+  
+}
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class BoolTest {
+    @Test
+    public void testBoolToWord() {
+        assertEquals(YesOrNo.boolToWord(true),"Yes");
+        assertEquals(YesOrNo.boolToWord(false),"No");
+    }
+}
