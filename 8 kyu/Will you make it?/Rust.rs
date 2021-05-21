d82abb55bf0e90dc4275fba3dce40fe2c7637d10
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-05-21 15:07:58 +0300

    Create Rust.rs

---
+++ 8 kyu/Will you make it?/Rust.rs
@@ -0,0 +1,18 @@
+fn zero_fuel(distance_to_pump: u32, mpg: u32, gallons: u32) -> bool {
+  if gallons * mpg >= distance_to_pump{
+      return true;
+    }
+    false
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::zero_fuel;
+    
+    #[test]
+    fn sample_tests() {
+      assert_eq!(zero_fuel(50, 25, 2), true);
+      assert_eq!(zero_fuel(100, 50, 1), false);
+    }
+}
