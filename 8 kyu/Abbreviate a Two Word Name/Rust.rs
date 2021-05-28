d789cd462e3b0a0e002940421afa6bf9ef8187f8
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-05-28 14:16:30 +0300

    Create Rust.rs

---
+++ 8 kyu/Abbreviate a Two Word Name/Rust.rs
@@ -0,0 +1,13 @@
+fn abbrev_name(name: &str) -> String {
+     name.split(" ").map(|i| i[0..1].to_uppercase()).collect::<Vec<_>>().join(".")
+}
+
+// Rust test example:
+#[test]
+fn sample_tests() {
+  assert_eq!(abbrev_name("Sam Harris"), "S.H");
+  assert_eq!(abbrev_name("Patrick Feenan"), "P.F");
+  assert_eq!(abbrev_name("Evan Cole"), "E.C");
+  assert_eq!(abbrev_name("P Favuzzi"), "P.F");
+  assert_eq!(abbrev_name("David Mendieta"), "D.M");
+}
