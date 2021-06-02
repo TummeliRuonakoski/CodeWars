fad1b167846e9537c1e269c87fed9182a21aca68
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-06-02 10:58:25 +0300

    Create Rust.rs

---
+++ 8 kyu/Count of positives sum of negatives/Rust.rs
@@ -0,0 +1,19 @@
+fn count_positives_sum_negatives(input: Vec<i32>) -> Vec<i32> {
+       
+     if input.is_empty(){
+          Vec::new()
+      } else{
+        vec![
+        input.iter().filter(|i| i.is_positive()).count() as i32, 
+        input.iter().filter(|i| i.is_negative()).sum()]
+    }
+}
+  
+  
+  
+#[test]
+fn returns_expected() {
+    let test_data1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+    let expected1 = vec![10, -65];
+    assert_eq!(count_positives_sum_negatives(test_data1), expected1);    
+}
