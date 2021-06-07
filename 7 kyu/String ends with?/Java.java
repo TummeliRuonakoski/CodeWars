a62737185fe5f32f2f8ae88084248bd78f7d7514
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-06-07 09:55:12 +0300

    Create Java.java

---
+++ 7 kyu/String ends with?/Java.java
@@ -0,0 +1,21 @@
+public class Kata {
+  public static boolean solution(String str, String ending) {
+    return str.endsWith(ending);
+  }
+}
+
+
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class SolutionTest {
+    @Test
+    public void testSomething() {
+        // assertEquals("expected", "actual");
+    }
+}
