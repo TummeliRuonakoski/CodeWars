215a355f9b152d90d10ebf21c8ef5ca7be52539c
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-06-09 14:01:24 +0300

    Create Java.java

---
+++ 7 kyu/Highest and Lowest/Java.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Kata {
+  public static String highAndLow(String numbers) {
+     
+   int max = Arrays.stream(numbers.split(" "))
+                    .mapToInt(i -> Integer.parseInt(i))
+                    .max()
+                    .getAsInt();
+    
+   int min = Arrays.stream(numbers.split(" "))
+                    .mapToInt(i -> Integer.parseInt(i))
+                    .min()
+                    .getAsInt();
+
+  
+
+    return String.format("%d %d", max, min); }
+}
+
+
+
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.*;
+
+public class KataTest {
+  @Test
+  public void test1() {
+    assertEquals("42 -9", Kata.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+  }
+}
