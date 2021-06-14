9579dbef4b818b169cb570f33205d847c0406375
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-06-14 14:02:19 +0300

    Create JavaScript.js

---
+++ 8 kyu/Remove First and Last Character/JavaScript.js
@@ -0,0 +1,18 @@
+function removeChar(str){
+return str.substring(1, str.length - 1);
+
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", () => {
+  it("Fixed Tests", () => {
+    assert.strictEqual(removeChar('eloquent'), 'loquen');
+    assert.strictEqual(removeChar('country'), 'ountr');
+    assert.strictEqual(removeChar('person'), 'erso');
+    assert.strictEqual(removeChar('place'), 'lac');
+    assert.strictEqual(removeChar('ooopsss'), 'oopss');
+  });
+});
