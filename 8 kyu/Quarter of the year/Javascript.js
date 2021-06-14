67ed26a473a8948c812fe72dec9ce461786db1ad
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-06-14 10:43:50 +0300

    Create Javascript.js

---
+++ 8 kyu/Quarter of the year/Javascript.js
@@ -0,0 +1,17 @@
+const quarterOf = (month) => {
+
+   return Math.ceil(month / 3);
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(quarter_of(3), 1, "Month 3 = quarter 1");
+        assert_eq!(quarter_of(8), 3, "Month 8 = quarter 3");
+        assert_eq!(quarter_of(11), 4, "Month 11 = quarter 4");
+    }  
+}
