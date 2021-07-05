fe98bf77cb6af20adccb3afb65261e4359743cbc
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-07-05 15:50:28 +0300

    Create Javascript.js

---
+++ 8 kyu/MakeUpperCase/Javascript.js
@@ -0,0 +1,10 @@
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+
+describe("Basic Tests", function(){
+  it("should pass the basic tests", function(){
+    Test.assertEquals(makeUpperCase("hello"), "HELLO");
+  });
+});
