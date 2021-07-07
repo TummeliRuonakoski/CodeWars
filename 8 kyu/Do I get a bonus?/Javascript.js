823e91a40a62c28deee1d747a696dbf267519711
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-07-07 13:43:13 +0300

    Create Javascript.js

---
+++ 8 kyu/Do I get a bonus?/Javascript.js
@@ -0,0 +1,34 @@
+
+
+function bonusTime(salary, bonus) {
+  if(bonus == true){
+    salary *= 10;
+  }
+  return "£" + salary;
+}
+
+
+
+function bonusTime(salary, bonus) {
+return '£' + (bonus ? salary * 10 : salary);
+}
+
+
+
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(bonusTime(10000, true), '£100000');
+    assert.strictEqual(bonusTime(25000, true), '£250000');
+    assert.strictEqual(bonusTime(10000, false), '£10000');
+    assert.strictEqual(bonusTime(60000, false), '£60000');
+    assert.strictEqual(bonusTime(2, true), '£20');
+    assert.strictEqual(bonusTime(78, false), '£78');
+    assert.strictEqual(bonusTime(67890, true), '£678900');
+  })
+})
