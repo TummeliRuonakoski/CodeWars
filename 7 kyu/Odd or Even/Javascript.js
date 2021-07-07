2b18f50a556134ecbbb5710434727fa9cdbfd328
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2021-07-07 16:39:01 +0300

    Create Javascript.js

---
+++ 7 kyu/Odd or Even/Javascript.js
@@ -0,0 +1,43 @@
+function oddOrEven(array) {
+  
+ return array.reduce( function (i, j) {
+   return i + j }, 0) % 2 == 0 ? "even": "odd"
+}
+
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe('Fixed tests', () => {
+  it('Edge tests', () => {
+    assert.strictEqual(oddOrEven([0]), 'even')
+    assert.strictEqual(oddOrEven([1]), 'odd')
+    assert.strictEqual(oddOrEven([]), 'even')
+  });
+  
+  it('Even tests', () => {
+    assert.strictEqual(oddOrEven([0, 1, 5]), 'even')
+    assert.strictEqual(oddOrEven([0, 1, 3]), 'even')
+    assert.strictEqual(oddOrEven([1023, 1, 2]), 'even')
+  });
+  
+  it('Negative Even tests', () => {
+    assert.strictEqual(oddOrEven([0, -1, -5]), 'even')
+    assert.strictEqual(oddOrEven([0, -1, -3]), 'even')
+    assert.strictEqual(oddOrEven([-1023, 1, -2]), 'even')
+  });
+  
+  it('Odd tests', () => {
+    assert.strictEqual(oddOrEven([0, 1, 2]), 'odd')
+    assert.strictEqual(oddOrEven([0, 1, 4]), 'odd')
+    assert.strictEqual(oddOrEven([1023, 1, 3]), 'odd')
+  });
+  
+  it('Negative Odd tests', () => {
+    assert.strictEqual(oddOrEven([0, -1, 2]), 'odd')
+    assert.strictEqual(oddOrEven([0, 1, -4]), 'odd')
+    assert.strictEqual(oddOrEven([-1023, -1, 3]), 'odd')
+  });
+});
